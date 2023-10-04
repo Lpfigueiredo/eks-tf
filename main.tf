934b3fa9de6f8b2f2684388e966828fc0bcfaa4d
Leonardo Figueiredo <leommko@gmail.com>
2023-10-04 12:03:13 -0300

    install argocd

--- main.tf
+++ main.tf
@@ -205,3 +205,21 @@ resource "helm_release" "alb-controller" {
     value = module.eks.cluster_name
   }
 }
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+# Install argocd helm chart using terraform
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "5.24.1"
+  namespace  = kubernetes_namespace.argocd.metadata.0.name
+  depends_on = [
+    kubernetes_namespace.argocd
+  ]
+}
