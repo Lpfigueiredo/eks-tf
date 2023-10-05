4236c3bac727beb9d7605b10eb420eceaeaac8da
Leonardo Figueiredo <leommko@gmail.com>
2023-10-04 23:11:39 -0300

    install latest argocd chart

--- main.tf
+++ main.tf
@@ -217,7 +217,6 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "5.24.1"
   namespace  = kubernetes_namespace.argocd.metadata.0.name
   depends_on = [
     kubernetes_namespace.argocd,
